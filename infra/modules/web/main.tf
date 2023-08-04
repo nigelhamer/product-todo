1a4cc6e935f5feee460c7556d3375431c73d8985
Nigel Hamer <nigel@hamersmith.co.uk>
2023-08-04 11:21:59 +0100

    Apply Fix (#4)

--- infra/modules/web/main.tf
+++ infra/modules/web/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_static_site" "web" {
   name                = var.name
   location            = "westeurope"
   resource_group_name = var.resource_group_name
-  tags                = var.tags
+  tags                = merge(var.tags, var.locator-tags)
   sku_size            = "Free"
 }
